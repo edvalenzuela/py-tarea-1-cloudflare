8e61090875f3d748adbe160ccb5c884faa1175f2
edvalenzuela <edvalenzuelap@gmail.com>
2023-01-16 18:42:27 -0500

    ✨ add method for dns

--- getHeaders.py
+++ getHeaders.py
@@ -4,6 +4,7 @@
 
 import requests
 import argparse
+import dns.resolver
 
 parser = argparse.ArgumentParser(description="Muestra información sobre Cloudfare y DNS")
 parser.add_argument("-t", "--target", type=str, default="https://www.google.com", help="sitio web para revisar header", required=True)
@@ -32,13 +33,31 @@ def obtenerHeaders(url):
             
         for k,v in resp.headers.items():
             print("K : {} + V :{}".format(k, v))
-    
+   
+def obtenerDns(url):
+    MYQUERY = "NS"
+    print ("realizando dns resolve en url : {} - {}".format(url, MYQUERY))
+    try:
+        result = dns.resolver.resolve(url,MYQUERY)
+        print(result)
+        for i in result:
+            result=i.to_text()
+            print('NS', result)
+            print("2nd step")
+        
+        word="cloudflare"
+        if word in result:
+            print("detectado clouflare en dns")
+        else:
+            print("no se encontro clouflare en dns")
+    except:
+        print("No se pudo acceder al Authoritative name server")
 
 if __name__ == "__main__":
     if parser.target and parser.method == 1:
         obtenerHeaders(parser.target)
     elif parser.target and parser.method == 2:
-        print("method 2!!!")
+        obtenerDns(parser.target)
     else :
         print("opciones de los argumentos incorrectas !!!")
 else :
